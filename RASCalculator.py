19442be6ad90e5b2e3f65bc0fd276cbf663a19eb
TCLamnidis <thisseass@gmail.com>
2018-01-31 21:02:51 +0100

    Intitial implementation of Missingness cutoff

--- RASCalculator.py
+++ RASCalculator.py
@@ -5,51 +5,6 @@ from math import sqrt
 from time import strftime
 import RASUtils as ras
 
-# class FreqSumParser:
-#     def __init__(self, filehandle):
-#         self.handle = filehandle
-#         self.__readHeader()
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         # while(True):
-#         line = next(self.handle)
-#         fields=line.strip().split()
-#         if fields[0][0:3] == "chr":
-#             Chrom = int(fields[0][3:])-1 #ignore "chr" if in start of chromosome name
-#         else
-#             Chrom=int(fields[0])-1 #Convert Chromosome numbering to 0-based)
-#         Pos=int(fields[1])
-#         Ref=fields[2]
-#         Alt=fields[3]
-#         AlleleFreqs = [int(x) for x in fields[4:]]
-#         afDict = dict(zip(self.popNames, AlleleFreqs))
-#         # if self.noTransitions and ...isTransition...:
-#         #     continue
-#         return (Chrom, Pos, Ref, Alt, afDict)
-#
-#     def __readHeader(self):
-#         line = next(self.handle)
-#         fields=line.strip().split()
-#         self.sizes = {}
-#         self.popNames = []
-#         Pops=fields[4:]
-#         for p in Pops:
-#             splitPopName = re.split('[(|)]', p)
-#             popName = splitPopName[0]
-#             self.popNames.append(popName)
-#             popSize = int(splitPopName[1])
-#             self.sizes[popName] = popSize
-#         print("#Available populations in Input File and their respective sizes: ", self.sizes, file=args.Output)
-#
-  
-  
-# def getJackknife(blockValues, blockSizes):
-#     ...return (jackknifeEstimator, jackknifeStdErr)
-
-  
 ########## MAIN ############
 
 parser = argparse.ArgumentParser(description="Extract the frequency of shared rare variants between each left population and all right populations from a freqsum file. Also preforms error estimation using jackknifing, using the number of observed sites for normalisation.")
@@ -59,6 +14,7 @@ parser.add_argument("-M", "--maxAF", metavar="<MAX ALLELE COUNT>", type=int, def
 parser.add_argument("-m", "--minAF", metavar="<MIN ALLELE COUNT>", type=int, default=2, help="The minimum number of alleles (total) in the reference populations. The default minimum allele count is 2.", required=False)
 parser.add_argument("-L", "--LeftPops", type=str, metavar="POP1,POP2,...", required=True, help="Set the Test populations/individuals. RAS will be calculated between the Test and all Right populations.")
 parser.add_argument("-R", "--RightPops", type=str, metavar="POP1,POP2,...", required=False, help="A list of comma-separated population names that should be considered when computing the allele frequency. Consider all populations if not provided.")
+parser.add_argument("-x", "--MissingnessCutoff", type=float, metavar="<CUTOFF>", default=0.0, help="Missingness cutoff proportion for Right populations. E.g. 0.1: If more than 10% of individuals in Right populations show missing data, the variant will be ignored. [default=0]")
 parser.add_argument("-NT", "--NoTransitions", action='store_true', help="When present, No Transitions are included in the output. Useful for ancient samples with damaged DNA.")
 parser.add_argument("-P", "--Private", action='store_true', required=False, help="Restrict the RAS calculation to privately shared rare variants only.")
 parser.add_argument("-C", "--NrChroms", type=int, metavar="<INT>", default=22, required=False, help="The number of chromosomes in the dataset. [22]")
@@ -99,6 +55,13 @@ RAS = [[[[0 for i in range(NumBins)] for j in range(maxAF+1)] for k in range(len
 mj = [[[[0 for i in range(NumBins)] for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
 
 for (Chrom, Pos, Ref, Alt, afDict) in freqSumParser:
+    missing=0
+    #Skip sites where missingness in rightpops is above specified cutoff.
+    for x in RightPops:
+        if afDict[x]==-1:
+            missing+=freqSumParser.sizes[x]
+    if missing/sum(freqSumParser.sizes)>=args.MissingnessCutoff:
+        continue
     #Exclude transitions if the option is given.
     if args.NoTransitions:
         if Ref in Transitions and Alt == Transitions[Ref]:
@@ -119,10 +82,9 @@ for (Chrom, Pos, Ref, Alt, afDict) in freqSumParser:
             if args.Private:
                 if AfSum != afDict[rightPop]:
                     continue
-            #Handle missing data by reducing pop size for that position (take out two chromosomes). If left is missing, count site. no RAS.
             leftSize=freqSumParser.sizes[leftPop]
             rightSize=freqSumParser.sizes[rightPop]
-             #For now we will assume that Rights cannot have missing data, since they are grouped. RAS calculation with some missingness in rights will come once we start grouping individuals in populations internally.
+            #For now we will assume that Rights cannot have missing data, since they are grouped. RAS calculation with some missingness in rights will come once we start grouping individuals in populations internally.
             # if afDict[rightPop] == -1:
             #     rightSize-=2
             
@@ -150,65 +112,11 @@ for i in range(minAF-1,maxAF+1):
             ThetaJ[x][j][i]=thetaJ
             Sigma2[x][j][i]=sigma2
 
-# ThetaJ = [[[getJackknife(RAS[leftIdx][rightIdx][af], mj[leftIdx][rightIdx][af])[0] for af in ...] for rightIdx in ...] for leftIdx in ...]
-# Sigma2 = [[[getJackknife(RAS[leftIdx][rightIdx][af], mj[leftIdx][rightIdx][af])[1] for af in ...] for rightIdx in ...] for leftIdx in ...]
-# alternative:
-# ThetaJ=[[[0 for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# Sigma2=[[[0 for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# for ...
-#     for ...
-#         for ...
-#             thetaJ, sigma2 = getJackknife(...)
-#             ThetaJ[][][] = thetaJ
-#             Sigma2[][][] = sigma2
-#
-
-# Thetahat = [[[0 for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# Thetaminus=[[[[0 for c in range(NumBins)] for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# for i in range(minAF-1,maxAF+1): # M+1 to pick up all frequencies (0-based to 1-based).
-#     for x in range(len(LeftPops)):
-#         for j in range(len(RightPops)):
-#             if sum(mj[x][j][i])==0:
-#                 Thetahat[x][j][i]=0
-#             else:
-#                 Thetahat[x][j][i]=(sum(RAS[x][j][i])/sum(mj[x][j][i]))
-#             for c in range(NumBins):
-#                 if mj[x][j][i][c] == sum(mj[x][j][i]):
-#                     Thetaminus[x][j][i][c]=0
-#                 else:
-#                     # Thetaminus[j][i][c]=(sum(RAS[j][i]) - RAS[j][i][c]) / (sum(lengths) - lengths[c])
-#                     Thetaminus[x][j][i][c]=(sum(RAS[x][j][i]) - RAS[x][j][i][c]) / (sum(mj[x][j][i]) - mj[x][j][i][c])
-#
-# ThetaJ=[[[0 for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# for i in range(minAF-1,maxAF+1): # M+1 to pick up all chromosomes (0-based to 1-based). mAF-1 to get the Thetas for the Sum of AFs too.
-#     for x in range(len(LeftPops)):
-#         for j in range(len(RightPops)):
-#             Sum1=0
-#             Sum2=0
-#             for c in range(NumBins):
-#                 Sum1+=Thetahat[x][j][i]-Thetaminus[x][j][i][c]
-#                 if sum(mj[x][j][i]) >0:
-#                     Sum2+=((mj[x][j][i][c] * Thetaminus[x][j][i][c])/sum(mj[x][j][i]))
-#             ThetaJ[x][j][i]=Sum1+Sum2
-#
-# Sigma2=[[[0 for j in range(maxAF+1)] for k in range(len(RightPops))] for x in range(len(LeftPops))]
-# for i in range(minAF-1,maxAF+1): # M+1 to pick up all chromosomes (0-based to 1-based). mAF-1 to get the Thetas for the Sum of AFs too.
-#     for x in range(len(LeftPops)):
-#         for j in range(len(RightPops)):
-#             for c in range(NumBins):
-#                 if mj[x][j][i][c]==0:
-#                     pseudovalue=Thetahat[x][j][i]
-#                     Sigma2[x][j][i]+=0
-#                 else:
-#                     hj=sum(mj[x][j][i])/mj[x][j][i][c]
-#                     pseudovalue=(hj*Thetahat[x][j][i])-((hj-1) * Thetaminus[x][j][i][c])
-#                     Sigma2[x][j][i]+=(((pseudovalue-ThetaJ[x][j][i])**2)/(hj-1))/NumBins
-#
 # print ("#FREQSUM POPULATIONS & SIZES:",*Pops, file=args.Output, sep=" ", end="\n")
 print ("#Left Populations: ", *LeftPops, sep=" ", file=args.Output, end="\n")
 print ("#Populations considered for allele frequency calculation (Rights):", *RightPops, file=args.Output, sep="\t", end="\n")
 # RAS, number of sites, RAS /Site, stderr of (RAS/site), Allele Freq
-print("RightPop","LeftPop","RAS","Number of sites","RAS/site", "Jackknife Error", "Allele Frequency", sep="\t", file=args.Output)
+print("RightPop","LeftPop","RAS","Number of sites","RAS/site JK Estimate", "Jackknife Error", "Allele Frequency", sep="\t", file=args.Output)
 for leftidx, leftPop in enumerate(LeftPops):
     for rightidx, rightPop in enumerate(RightPops):
         if args.details:
